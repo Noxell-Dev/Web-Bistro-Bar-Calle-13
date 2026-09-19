c7aec3cba07b8cb049659d0e271bcc22508486a7
Diego <69425536+dmartin015@users.noreply.github.com>
2026-09-19 13:35:23 +0200

    Jamones al penúltimo puesto de Tapas

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -49,8 +49,6 @@ export const menu: MenuCategory[] = [
     id: "tapas",
     title: "Tapas",
     items: [
-      { name: "Jamón de cebo", price: "21,00 €" },
-      { name: "Jamón de bellota", price: "24,00 €" },
       { name: "Montadito de pollo/lomo", price: "2,50 €" },
       { name: "Montadito Chicharrones", price: "3,00 €" },
       { name: "Pinchito de pollo", price: "3,00 €" },
@@ -60,6 +58,8 @@ export const menu: MenuCategory[] = [
         name: "Queso Camembert frito con mermelada de frambuesa",
         price: "4,50 €",
       },
+      { name: "Jamón de cebo", price: "21,00 €" },
+      { name: "Jamón de bellota", price: "24,00 €" },
       { name: "Salsa extra", price: "0,50 €" },
     ],
   },
